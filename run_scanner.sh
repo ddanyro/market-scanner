68a2e027035b1a27bd244eb6646c406199b46f5f
ddany <daniel.dragomir@gmail.com>
2025-12-24 12:06:44 +0200

    Enable TWS sync in cron job

--- run_scanner.sh
+++ run_scanner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /Users/danieldragomir/antigravity
-/usr/local/bin/python3 market_scanner.py >> dashboard.log 2>&1
+/usr/local/bin/python3 market_scanner.py --mode all --tws >> dashboard.log 2>&1
