b5dc6ec82095ad4da91e26887d5e71f984f2a5ad
GitHub Action <action@github.com>
2026-07-30 20:27:25 +0000

    Auto-update Dashboard [skip ci]

--- push/firebase/firebase-messaging-sw.js
+++ push/firebase/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-/*
- * Acest fișier este regenerat de market_scanner.py când variabila
- * FIREBASE_WEB_CONFIG este configurată. Configurația web Firebase este publică;
- * cheia privată a contului de serviciu nu este scrisă niciodată aici.
- */
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({"messagingSenderId": "639451643472", "appId": "1:639451643472:web:a2b4cb96d79114f0509b55", "projectId": "alerte-buy", "measurementId": "G-LQ1HK4MQ65", "apiKey": "AIzaSyBURLqUMnuY9_84obaWBbI8OEjDKa0Ug2E", "storageBucket": "alerte-buy.firebasestorage.app", "authDomain": "alerte-buy.firebaseapp.com"});
+firebase.messaging();
