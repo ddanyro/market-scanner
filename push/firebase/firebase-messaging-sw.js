ee27d12e1f5c2c3f25348b86b60dc0e30de53b65
GitHub Action <action@github.com>
2026-08-07 15:13:24 +0000

    Auto-update Romanian market

--- push/firebase/firebase-messaging-sw.js
+++ push/firebase/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
 
-firebase.initializeApp({"measurementId": "G-LQ1HK4MQ65", "projectId": "alerte-buy", "authDomain": "alerte-buy.firebaseapp.com", "messagingSenderId": "639451643472", "storageBucket": "alerte-buy.firebasestorage.app", "appId": "1:639451643472:web:a2b4cb96d79114f0509b55", "apiKey": "AIzaSyBURLqUMnuY9_84obaWBbI8OEjDKa0Ug2E"});
+firebase.initializeApp({"apiKey": "AIzaSyBURLqUMnuY9_84obaWBbI8OEjDKa0Ug2E", "projectId": "alerte-buy", "appId": "1:639451643472:web:a2b4cb96d79114f0509b55", "measurementId": "G-LQ1HK4MQ65", "messagingSenderId": "639451643472", "authDomain": "alerte-buy.firebaseapp.com", "storageBucket": "alerte-buy.firebasestorage.app"});
 firebase.messaging();
