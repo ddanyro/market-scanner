e180481345fae8e1672bd422710a3a4216a6648e
GitHub Action <action@github.com>
2026-08-11 11:17:03 +0000

    Auto-update Dashboard [skip ci]

--- push/firebase/firebase-messaging-sw.js
+++ push/firebase/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
 
-firebase.initializeApp({"storageBucket": "alerte-buy.firebasestorage.app", "projectId": "alerte-buy", "appId": "1:639451643472:web:a2b4cb96d79114f0509b55", "messagingSenderId": "639451643472", "authDomain": "alerte-buy.firebaseapp.com", "apiKey": "AIzaSyBURLqUMnuY9_84obaWBbI8OEjDKa0Ug2E", "measurementId": "G-LQ1HK4MQ65"});
+firebase.initializeApp({"apiKey": "AIzaSyBURLqUMnuY9_84obaWBbI8OEjDKa0Ug2E", "measurementId": "G-LQ1HK4MQ65", "messagingSenderId": "639451643472", "authDomain": "alerte-buy.firebaseapp.com", "appId": "1:639451643472:web:a2b4cb96d79114f0509b55", "storageBucket": "alerte-buy.firebasestorage.app", "projectId": "alerte-buy"});
 firebase.messaging();
